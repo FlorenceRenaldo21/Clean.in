8a45236f86efc39c7fe88a880a6e0fe6c92ad92f
yohanes andres <yohanes141002@mhs.mdp.ac.id>
2022-12-22 17:00:59 +0700

    add project

---
+++ Android/app/src/main/java/Com/NEKAT/CleanIn/MainActivity.java
@@ -0,0 +1,14 @@
+package com.Nekat.CleanIn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
